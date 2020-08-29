21da908f464a7f723fe4522aa87154dea8bdad46
mrkeksz <mrkeksz@yandex.ru>
2020-08-29 09:59:53 +0300

    init

---
+++ src/Todo/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+const styles = {
+  ul: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+  },
+}
+
+export default function TodoList(props) {
+  return (
+    <ul style={styles.ul}>
+      {props.todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />
+      })}
+    </ul>
+  )
+}
